135c3c79879e7ca1e58028d8ed4d815833d61d50
thiago <deoliveirasantosthiago4@gmail.com>
2023-04-03 19:42:25 -0300

    primeiro commit.

---
+++ index.js
@@ -0,0 +1,56 @@
+const inputPergunta = document.getElementById("pergunta")
+const inputResposta = document.getElementById("resposta")
+const OPEN_API_KEY = "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"
+
+inputPergunta.addEventListener("keypress", (e) => {
+    if(inputPergunta.value && e.key === "Enter"){
+        buscaPergunta();
+    }
+})
+
+function buscaPergunta(){
+    
+    let sPergunta = inputPergunta.value;
+
+    fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + OPEN_API_KEY,
+        },
+        body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt: sPergunta,
+            max_tokens: 2048,
+            temperature: 0.5,
+        })
+    })
+    .then((inputResposta) => inputResposta.json())
+    .then((json) => {
+
+        inputPergunta.value = "Carregando..."
+        inputPergunta.disabled = true;
+
+        if(inputResposta.value) inputResposta.value += "\n"
+
+        if(json.error?.message){
+            inputResposta.value += `Error: ${json.error.mesage}`
+        }else if(json.choices?.[0].text) {
+            var text = json.choices[0].text || "Sem Resposta";
+            inputResposta.value += `ChatGpt: ${text}`;
+        }
+        inputResposta.scrollTop = inputResposta.scrollHeight;
+    })
+    .catch((error) => console.error("Error:",error))
+    .finally(() => {
+        inputPergunta.value = "";
+        inputPergunta.disabled = false;
+        inputPergunta.focus();
+    })
+
+    if(inputResposta.value) inputResposta.value += "\n\n\n";
+    inputResposta.value += `Eu: ${sPergunta}`
+
+    inputResposta.scrollTop = inputResposta.scrollHeight;
+}
